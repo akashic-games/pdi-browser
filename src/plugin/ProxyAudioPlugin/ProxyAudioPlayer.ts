d6935973a3bf8d07835c511ee0c60449618bba82
xnv <xnv@users.noreply.github.com>
2019-08-07 15:27:39 +0900

    ProxyAudioPluginのstoppedの簡易対応を削除

--- src/plugin/ProxyAudioPlugin/ProxyAudioPlayer.ts
+++ src/plugin/ProxyAudioPlugin/ProxyAudioPlayer.ts
@@ -8,16 +8,12 @@ export class ProxyAudioPlayer extends g.AudioPlayer implements AudioPlayer {
 	private _audioPlayerId: string | null;
 	private _handlerSet: ProxyAudioHandlerSet;
 	private _manager: AudioManager;
-	private _durationWaitTimer: any;
-	private _handleDurationPassed_bound: () => void;
 
 	constructor(handlerSet: ProxyAudioHandlerSet, system: g.AudioSystem, manager: AudioManager) {
 		super(system);
 		this._audioPlayerId = null;
 		this._handlerSet = handlerSet;
 		this._manager = manager;
-		this._durationWaitTimer = null;
-		this._handleDurationPassed_bound = () => this._handleDurationPassed();
 	}
 
 	changeVolume(volume: number): void {
@@ -37,14 +33,12 @@ export class ProxyAudioPlayer extends g.AudioPlayer implements AudioPlayer {
 		this._audioPlayerId = this._handlerSet.createAudioPlayer(asset.id);
 		this._handlerSet.changeAudioVolume(this._audioPlayerId, this._calculateVolume());
 		this._handlerSet.playAudioPlayer(this._audioPlayerId);
-		this._durationWaitTimer = setTimeout(this._handleDurationPassed_bound, asset.duration);
 		super.play(asset);
 	}
 
 	stop(): void {
 		if (this._audioPlayerId != null) {
-			this._clearEndedEventHandler();
-			this._handlerSet.stopAudioPlayer(this._audioPlayerId); // 不要？
+			this._handlerSet.stopAudioPlayer(this._audioPlayerId);
 			this._handlerSet.destroyAudioPlayer(this._audioPlayerId);
 			this._audioPlayerId = null;
 		}
@@ -53,17 +47,6 @@ export class ProxyAudioPlayer extends g.AudioPlayer implements AudioPlayer {
 
 	notifyMasterVolumeChanged(): void {
 		this._notifyVolumeToHandler();
-	}
-
-	private _handleDurationPassed(): void {
-		this.stop();
-	}
-
-	private _clearEndedEventHandler(): void {
-		if (this._durationWaitTimer != null) {
-			clearTimeout(this._durationWaitTimer);
-			this._durationWaitTimer = null;
-		}
 	}
 
 	private _notifyVolumeToHandler(): void {
