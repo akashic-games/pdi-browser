d06cd0bac6eab46c34270b7c49a684cd87464f48
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2021-02-09 15:06:28 +0900

    Fix lint error

--- src/plugin/AudioPluginRegistry.ts
+++ src/plugin/AudioPluginRegistry.ts
@@ -3,7 +3,8 @@ import { AudioPluginStatic } from "./AudioPluginStatic";
 
 var audioPlugins: AudioPluginStatic[] = [];
 
-export var AudioPluginRegistry = {
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export var AudioPluginRegistry = { // export してるため命名規則の lint を除外
 	addPlugin(plugin: AudioPluginStatic): void {
 		if (audioPlugins.indexOf(plugin) === -1) {
 			audioPlugins.push(plugin);
