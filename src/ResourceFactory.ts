b08b76b2b68fd74c7705cb6be458ccc7a377565c
yu-ogi <yusuke_ogiwara@dwango.co.jp>
2022-08-02 17:12:14 +0900

    chore: drop unused variable

--- src/ResourceFactory.ts
+++ src/ResourceFactory.ts
@@ -25,7 +25,6 @@ export class ResourceFactory implements pdi.ResourceFactory {
 	_rendererCandidates: string[];
 	_surfaceFactory: SurfaceFactory;
 	_platform: Platform;
-	_dynamicGeneratedAssetsCount: number = 0;
 
 	constructor(param: ResourceFactoryParameterObject) {
 		this._audioPluginManager = param.audioPluginManager;
